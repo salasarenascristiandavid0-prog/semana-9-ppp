a71e9fbfb1f4a7b0bf3c133d92d3c1c7c49e8b60
cris <salasarenascristiandavid0@gmail.com>
2026-05-04 19:03:08 -0500

    Actividad semana 2

---
+++ semana2/comandos-terminal.py
@@ -0,0 +1,11 @@
+mkdir semana2
+~ $ cd semana2
+~/semana2 $ touch preguntas-semana2.txt
+~/semana2 $ touch comandos-terminal.txt
+~/semana2 $ touch reflexion semana2.txt
+~/semana2 $ nano preguntas-semana2.py
+~/semana2 $ nano preguntas-semana2.py
+~/semana2 $ nano reflexion-semana2.py
+~/semana2 $ nano comandos-terminal.py
+
+
